f83521d60105a6f584ee39d8cc1067a2465aa14c
Marvin Zumbado <marvinzzz@gmail.com>
2022-05-06 15:01:58 -0600

    added logic and images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'garage';
+  // currentCustomer = 'marvin';
+  garage1 = true;
+  garage2 = true;
+
+  notProgrammed () {
+    alert('This button is not programmed yet');
+  }
 }
